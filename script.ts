82680985869b60d109678bf514ccd55d911dc312
bakedPotatoLord <918780@dpsk12.net>
2023-03-19 18:39:51 -0600

    move note class back to script.ts

--- script.ts
+++ script.ts
@@ -1,4 +1,3 @@
-import Note  from "./Note"
 
 //get HTML elements
 let noteInput = <HTMLElement>document.querySelector('.left textarea')
@@ -43,3 +42,29 @@ function deleteNote(id:number){
 }
 
 window.onload = displayNotes
+
+class Note{
+	note:string
+	date: Date
+	id:number
+	constructor(note:string){
+	//constructs note
+	this.note = note
+	this.date = new Date()
+	this.id = Date.now()
+	}
+	//a method that all Note objects have
+	generateHTML(){
+		//returns note in stylable HTML form
+		return `
+		<div class='note'>
+		<div class='text'>
+			<a>${this.date.getMonth()}/${this.date.getDate()}, ${this.date.getHours()}:
+			${this.date.getMinutes()}</a><br>
+			<a>${this.note}<a>
+		</div>
+		<button onclick="deleteNote(${this.id})">Delete Note</button>
+		</div>
+		`
+	}
+}
